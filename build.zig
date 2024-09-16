f6a5a461bbe0cecbcfabd36475431ba835a0ff2d
Alex Loiko <alexandreloiko@gmail.com>
2024-09-16 16:26:22 +0200

    Run all tests though `zig build test`

--- build.zig
+++ build.zig
@@ -64,30 +64,27 @@ pub fn build(b: *std.Build) void {
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
-    // Creates a step for unit testing. This only builds the test executable
-    // but does not run it.
-    const lib_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/mini.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
-
-    const exe_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/mini.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
-
     // Similar to creating the run step earlier, this exposes a `test` step to
     // the `zig build --help` menu, providing a way for the user to request
     // running the unit tests.
     const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_lib_unit_tests.step);
-    test_step.dependOn(&run_exe_unit_tests.step);
+
+    const test_files = [_][]const u8 {
+        "src/parse_utils.zig",
+        "src/format.zig",
+        "src/mini.zig"
+    };
+
+    inline for (test_files) |test_file| {
+        const unit_test = b.addTest(.{
+            .root_source_file = b.path(test_file),
+            .target = target,
+            .optimize = optimize,
+        });
+        const run_unit_test = b.addRunArtifact(unit_test);
+        test_step.dependOn(&run_unit_test.step);
+    }
+
 
     const exe_check = b.addExecutable(.{
         .name = "foo",
